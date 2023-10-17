342ea807b64b70701fcaa5ebf2d8ab55010933ea
Salma2486 <saloom_001@hotmail.com>
2023-10-17 11:43:36 -0700

    shell.h

--- shell.h
+++ shell.h
@@ -5,5 +5,12 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+void prompt(void);
+void clean_input(char *in ,char * out);
+char* read_input(void);
+int extract_args(char *in, char *args[16]);
+void free_args(char *args[16],int n);
+void print_environment(void);
+int execute(char *cmd);
 
 #endif
